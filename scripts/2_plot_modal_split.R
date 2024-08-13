cbd0e338d2abdb124b023d03e7f97f9272c716f4
Julian Vogels <julian.vogels@mailbox.org>
2024-08-13 19:50:59 +0200

    Add color scheme to modal split, sort legend by largest value

--- scripts/2_plot_modal_split.R
+++ scripts/2_plot_modal_split.R
@@ -1,6 +1,12 @@
 # Load the data from the RDS file
 all_data <- readRDS("output/all_data.rds")
 
+# Get the unique transport modes present in the dataset
+unique_transport_modes <- unique(all_data$`Means of transportation`)
+
+# Generate transport group colors dynamically based on present groups
+transport_colors <- get_transport_colors(unique_transport_modes)
+
 # Function to process modal split data using single journey distance
 process_modal_split_data <- function(data) {
   modal_split_data <- data %>%
@@ -20,6 +26,15 @@ process_modal_split_data <- function(data) {
 # Process modal split data
 modal_split_data <- process_modal_split_data(all_data)
 
+# Reorder the levels of Means of transportation based on the highest to lowest percentage
+modal_split_data <- modal_split_data %>%
+  mutate(`Means of transportation` = factor(`Means of transportation`, 
+                                            levels = modal_split_data %>%
+                                              group_by(`Means of transportation`) %>%
+                                              summarize(Total = sum(Percentage)) %>%
+                                              arrange(desc(Total)) %>%
+                                              pull(`Means of transportation`)))
+
 # Filter out any labels with Percentage <= 1%
 modal_split_data_filtered <- modal_split_data %>% filter(Percentage > 1)
 
@@ -29,6 +44,8 @@ plot <- ggplot(modal_split_data, aes(x = "", y = Percentage, fill = `Means of tr
   coord_polar("y", start = 0) +
   facet_wrap(~Location) +
   theme_minimal() +
+  custom_theme +
+  scale_fill_manual(values = transport_colors) +
   labs(title = "Modal Split",
        subtitle = "Share of transport performance",
        x = NULL,
@@ -37,13 +54,16 @@ plot <- ggplot(modal_split_data, aes(x = "", y = Percentage, fill = `Means of tr
         axis.ticks = element_blank(),   # Remove axis ticks
         panel.grid = element_blank(),   # Remove grid lines
         plot.title = element_text(hjust = 0.5),  # Center title
-        plot.subtitle = element_text(hjust = 0.5)) +  # Center subtitle
+        plot.subtitle = element_text(hjust = 0.5),
+        legend.title = element_blank()) +  # Center subtitle and hide legend title
   geom_label_repel(data = modal_split_data_filtered,  # Use filtered data for labels
                    aes(label = sprintf("%.1f%%", Percentage)),
                    position = position_stack(vjust = 0.5),
                    show.legend = FALSE,  # Hide legend for labels
                    box.padding = 0.3,
                    point.padding = 0.3,
-                   segment.color = 'grey50') 
+                   segment.color = 'grey50')
 
+# Save and print the plot
+ggsave("output/modal_split_plot.png", plot, width = 10, height = 6)
 print(plot)
--- scripts/load/load_plot_styles.R
+++ scripts/load/load_plot_styles.R
@@ -1,27 +1,74 @@
 # European Climate Pact color scheme
-custom_colors <- c(
-  color1 = "#4463E0",
-  color2 = "#E25449",
-  color3 = "#4FAF4C",
-  color4 = "#B34922",
-  color5 = "#377370",
-  color6 = "#A6BDF5",
-  color7 = "#EAA292",
-  color8 = "#99E492",
-  color9 = "#F1A23A",
-  color10 = "#CFA8F9",
-  color11 = "#71C3AF",
-  color12 = "#D7E4FA",
-  color13 = "#EDD7D4",
-  color14 = "#D5FDD5",
-  color15 = "#F6C87F",
-  color16 = "#E8D7FC",
-  color17 = "#B6F4E0"
+repeat_colors <- function(n) {
+  colors <- c("#4463E0", "#E25449", "#4FAF4C", "#B34922", "#377370", "#A6BDF5", 
+              "#EAA292", "#99E492", "#F1A23A", "#CFA8F9", "#71C3AF", "#D7E4FA",
+              "#EDD7D4", "#D5FDD5", "#F6C87F", "#E8D7FC", "#B6F4E0")
+  return(unname(rep(colors, length.out = n)))
+}
+
+# Define a color mapping for all possible transportation modes
+transport_mode_colors <- c(
+  "automobile" = "#4463E0",
+  "flight" = "#E25449",
+  "trainLongDistance" = "#4FAF4C",
+  "trainRegional" = "#B34922",
+  "trainUrban" = "#377370",
+  "busLocal" = "#A6BDF5",
+  "busLongDistance" = "#EAA292",
+  "cycling" = "#99E492",
+  "taxi" = "#F1A23A",
+  "tram" = "#CFA8F9",
+  "ferry" = "#71C3AF",
+  "pedelec" = "#D7E4FA",
+  "walking" = "#EDD7D4",
+  "motorScooterTwoStroke" = "#D5FDD5",
+  "motorScooterElectric" = "#F6C87F",
+  "motorbike" = "#E8D7FC",
+  "kickScooterElectric" = "#B6F4E0" 
 )
 
-# Function to create a repeated color palette
-repeat_colors <- function(n) {
-  rep(custom_colors, length.out = n)
+# Function to get colors for transportation modes present in the dataset
+get_transport_colors <- function(transport_modes) {
+  # Filter the colors to only those present in the dataset
+  filtered_colors <- transport_mode_colors[transport_modes]
+
+  # Return unnamed vector of colors
+  return(unname(filtered_colors))
+}
+
+# Define a color mapping for the transportation groups
+transport_group_colors <- c(
+  "Motorized Private Transport" = "#4463E0",
+  "Public Transport" = "#4FAF4C",
+  "Non-Motorized Private Transport" = "#F1A23A",
+  "Flights" = "#E25449"
+)
+
+# Function to group transportation modes into categories
+group_transport_modes <- function(mode) {
+  if (mode %in% c("automobile", "motorbike", "motorScooterTwoStroke", "motorScooterElectric", "taxi")) {
+    return("Motorized Private Transport")
+  } else if (mode %in% c("busLongDistance", "busLocal", "trainLongDistance", "trainRegional", "trainUrban", "tram", "ferry")) {
+    return("Public Transport")
+  } else if (mode %in% c("pedelec", "walking", "cycling")) {
+    return("Non-Motorized Private Transport")
+  } else if (mode %in% c("flight")) {
+    return("Flights")
+  } else {
+    return(NA)
+  }
+}
+
+# Function to get colors for transportation groups present in the dataset
+get_transport_group_colors <- function(transport_modes) {
+  # Group the transportation modes into categories
+  transport_groups <- unique(sapply(transport_modes, group_transport_modes))
+  
+  # Filter the colors to only those groups present in the dataset
+  filtered_colors <- transport_group_colors[transport_groups]
+  
+  # Return unnamed vector of colors
+  return(unname(filtered_colors))
 }
 
 # Define a custom font family
